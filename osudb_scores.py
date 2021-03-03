d77d8899122b9cd44489b9ce0f14223407e98137
pohmx <34806678+pohmx@users.noreply.github.com>
2021-03-03 03:28:51 -0300

    Add files via upload

---
+++ osudb_scores.py
@@ -0,0 +1,143 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pickle
+import os.path
+import time
+import json
+
+with open('C:/Users/nimi/Documents/Scripts/osudb_scores/login.json') as f:
+  data = json.load(f)
+
+username = data["username"]
+password = data["password"]
+
+# Cookies
+
+def save_cookie(driver, path):
+    with open(path, 'wb') as filehandler:
+        pickle.dump(driver.get_cookies(), filehandler)
+
+def load_cookie(driver, path):
+     with open(path, 'rb') as cookiesfile:
+         cookies = pickle.load(cookiesfile)
+         for cookie in cookies:
+             driver.add_cookie(cookie)
+
+# Navigate to osu
+
+dummy = "https://osu.ppy.sh/beatmapsets/"
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(executable_path="C:/WebDriver/bin/chromedriver", options=options)
+cookie = "C:/WebDriver/cookies/osu.pkl"
+driver.get("http://osu.ppy.sh")
+
+if not os.path.exists(cookie):
+
+    login = driver.find_element(By.XPATH, "/html/body/div[3]/nav/div[2]/div[2]/a").click()
+    user_field = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/form/div[1]/input[1]").send_keys(username + Keys.ENTER)
+    password_field = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/form/div[1]/input[2]").send_keys(password + Keys.ENTER)
+    time.sleep(3) # Make sure cookie is saved properly
+    save_cookie(driver, cookie)
+    exit()
+
+load_cookie(driver, cookie)
+ 
+# Beatmap testing
+
+b_sets = ["958476"]
+b_ids = ["2006816"]
+mod_container_path = "/html/body/div[7]/div/div/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[3]/div[6]/div[2]"
+unselected_mod = "[@class='beatmap-scoreboard-mod']"
+score_path = "/html/body/div[7]/div/div/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[1]/div/div[2]"
+acc_path = "/html/body/div[7]/div/div/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div[2]"
+pp_path = "/html/body/div[7]/div/div/div[3]/div[1]/div/div[3]/div/div[1]/div[2]/div/div/div[2]/div[3]/div[4]/div[2]/span"
+NM = ["NM","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[1]"]
+EZ = ["EZ","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[2]"]
+NF = ["NF","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[3]"]
+HT = ["HT","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[4]"]
+HR = ["HR","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[5]"]
+SD = ["SD","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[6]"]
+PF = ["PF","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[7]"]
+DT = ["DT","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[8]"]
+NC = ["NC","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[9]"]
+HD = ["HD","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[10]"]
+FL = ["FL","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[11]"]
+SO = ["SO","/html/body/div[7]/div/div/div[3]/div[1]/div/div[2]/button[12]"]
+wait = WebDriverWait(driver, 2) 
+
+def score_data():
+    SCORE = wait.until(EC.presence_of_element_located((By.XPATH, score_path))).text
+    ACC = wait.until(EC.presence_of_element_located((By.XPATH, acc_path))).text
+    PP = wait.until(EC.presence_of_element_located((By.XPATH, pp_path))).text
+    print(SCORE, ACC, PP)
+
+def mod(mod, single=True, override=False, disable=False):
+    if disable is False:
+        MOD = driver.find_element(By.XPATH, mod[1])
+        MOD.click()
+        if single is True:
+            try:
+                if mod is NM:
+                    wait.until(EC.presence_of_element_located((By.XPATH, mod_container_path)))
+                else:
+                    wait.until(EC.presence_of_element_located((By.XPATH, mod_container_path+"/div[@class='mod mod--"+mod[0]+"']")))
+                print(mod[0]+"--OK")
+                score_data()                
+            except:
+                print("--NO SCORE")
+            MOD.click()
+            wait.until(EC.presence_of_element_located((By.XPATH, mod[1]+unselected_mod)))
+        elif override is True: 
+            try:          
+                wait.until(EC.presence_of_element_located((By.XPATH, mod_container_path+"/div[@class='mod mod--"+mod[0]+"']")))
+                print("override--OK")
+                score_data()
+            except:
+                print("--NO SCORE")
+        else:
+            try:
+                wait.until(EC.presence_of_element_located((By.XPATH, mod_container_path+"/div[@class='mod mod--"+mod[0]+"']")))
+            except:
+                print("--??")
+    else: 
+        MOD = driver.find_element(By.XPATH, mod[1])
+        MOD.click()
+        wait.until(EC.presence_of_element_located((By.XPATH, mod[1]+unselected_mod)))
+
+def multi_mod(*args):
+    for arg in args[:-1]:
+        mod(arg, single=False)
+    mod(args[-1], single=False, override=True)
+    for arg in args:
+        mod(arg, disable=True)
+
+def check_single(*args):
+    for arg in args:
+        mod(arg)
+
+def check_multi(*mods):
+    for mod in mods:
+        s = ""
+        for name in mod:
+            s += str(name[0])
+        print("echo--",s)
+        multi_mod(*mod)
+
+for b_set in b_sets:
+    driver.get(dummy+b_set)   
+    for b_id in b_ids:
+        # Switch difficulty
+        driver.find_element(By.XPATH, '//a[@href="#taiko/'+b_id+'"]').click()
+        # Wait for mod's objects
+        wait.until(EC.presence_of_element_located((By.XPATH, NM[1])))
+        # Single Mod
+        check_single(NM,EZ,NF,HR,SD,DT,HD,FL)
+        # Multi Mod
+        check_multi((HD,HR),(EZ,DT),(EZ,HD),(EZ,FL),(HD,FL),(HR,FL),(DT,FL),(DT,HR),(HD,DT),(EZ,HD,FL),(EZ,DT,HD),(EZ,DT,FL),(HD,DT,HR),(HD,DT,FL),(DT,HR,FL),(HD,HR,FL),(EZ,HD,DT,FL),(HR,HD,DT,FL))
+
+x = input("close")
